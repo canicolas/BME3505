4b65608c92b4120a0e803afefeb102dca2f7565b
canicolas <66385219+canicolas@users.noreply.github.com>
2021-09-15 15:56:23 -0400

    correct

--- MatlabAnalysis.m
+++ MatlabAnalysis.m
@@ -4,4 +4,3 @@ close all;
 filename= input('Name of the file: ','s');
 fileID= readMyData(filename);
 
-end
--- readMyData.m
+++ readMyData.m
@@ -1,6 +1,30 @@
-clear all;
-close all;
 
-filename= input('Name of the file: ','s');
-S1= readMyData(filename);
+function data=readMyData(fileName)
+% Author: Siamak Ghorbani Faal
+% Date: 09/21/2014
+% Copyright @ Siamak Ghorbani Faal. All rights reserved
 
+
+fileID = fopen(fileName);
+formatData = '"%f""%f""%f""%f""%f"';
+
+n = 1;
+
+while(1)
+    string = fscanf(fileID,'%s%[^\r]%');
+    string = strrep(string,',','');
+    
+    if(isempty(string))
+        break
+    end
+    
+    vector = sscanf(string,formatData)';
+    
+    if(~isempty(vector))
+        data(n,:) = vector;
+        n = n + 1;
+    end
+    
+end
+
+fclose(fileID);
